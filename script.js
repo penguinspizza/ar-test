35ff421e4ce775ddc6878a2ac70afcf888e63f67
penguinspizza <72650078+penguinspizza@users.noreply.github.com>
2020-12-31 06:32:59 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("request_permission").addEventListener("click", function(){
+if (
+DeviceMotionEvent &&
+DeviceMotionEvent.requestPermission &&
+typeof DeviceMotionEvent.requestPermission === 'function'
+) {
+DeviceMotionEvent.requestPermission();
+}
+if (
+DeviceOrientationEvent &&
+DeviceOrientationEvent.requestPermission &&
+typeof DeviceOrientationEvent.requestPermission === 'function'
+) {
+DeviceOrientationEvent.requestPermission();
+}
+})
